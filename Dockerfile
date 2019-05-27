708cbace11f750f39279ea5dbf0ed6615777b6bc
Justin <fallingreign@gmail.com>
2019-05-27 10:54:10 +1000

    Handle UID in init instead of Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,24 @@
 FROM    python:3.6-alpine
 
-# Environment Vars
-ENV     PUID 1000
-ENV     PGID 1000
-ENV     TZ Australia/Melbourne
-
-# Update timezone
+# Install apks
 RUN     apk add --no-cache tzdata
-RUN     ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime \
-        && echo ${TZ} > /etc/timezone
-
-# Create a user
-RUN     adduser -D -u ${PUID} -g ${PGID} flexget
-WORKDIR /home/flexget/
-
-# Volumes
-VOLUME  /config
 
 # Install flexget & plugins
 RUN     pip3 install -U pip && pip3 install -U \
         flexget \
         deluge_client \
         subliminal>=2.0
-   
-# Add init script
-COPY    etc/init.sh /home/flexget/
-RUN     chmod +x ./init.sh
 
-USER    flexget
-CMD     ["./init.sh"]
+# Volumes
+VOLUME  /config
+WORKDIR /config
+
+# Add init script
+COPY    etc/init.sh /scripts/init.sh
+RUN     chmod +x /scripts/init.sh
+
+# App entry point
+CMD     ["/scripts/init.sh"]
 
 # Flexget web interface
 EXPOSE  5050/tcp
--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Timezone setting
+if [ -n "${TZ}" ]; then
+  echo "Set timezone to ${TZ}"
+  echo "${TZ}" > /etc/timezone
+  cp /usr/share/zoneinfo/"${TZ}" /etc/localtime
+fi
+
 # Config lockfile
 if [ -f /config/.config-lock ]; then
   echo "Removing .config-lock"
@@ -11,6 +18,13 @@ fi
 mkdir -p /config/logs/
 touch /config/logs/logfile.log
 
+# Set User permissions
+if [ -n "${PUID}" ] && [ -n "${PGID}" ]; then
+  adduser -D -H -s /bin/sh -u ${PUID} flexget flexget
+  chown -R "${PUID}":"${PGID}" /config
+  chmod -R 775 /config
+fi
+
 # Set FLEXGET_WEB_PASS
 if [[ ! -z "${FLEXGET_WEB_PASS}" ]]; then
   echo "Setting FLEXGET_WEB_PASS: ${FLEXGET_WEB_PASS}"
@@ -20,7 +34,13 @@ if [[ ! -z "${FLEXGET_WEB_PASS}" ]]; then
 fi
 
 # Run flexget
-flexget -c "/config/config.yml" \
-  --logfile "/config/logs/logfile.log" \
-  --loglevel "${FLEXGET_LOG_LEVEL:-debug}" \
-  daemon start --autoreload-config
+if [ -n "${PUID}" ]; then
+  echo "Starting Flexget..."
+  su flexget -m -c \
+    exec 'flexget -c "/config/config.yml" \
+    --logfile "/config/logs/logfile.log" \
+    --loglevel "${FLEXGET_LOG_LEVEL:-debug}" \
+    daemon start --autoreload-config'
+else
+  echo "Flexget needs a PUID & PGID environment variable"
+fi
